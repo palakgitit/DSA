208e6394098b5f721af24354b49ccd698ea48b04
palakgitit <palkzzzy@gmail.com>
2025-07-25 09:49:06 +0530

    commit

--- project_5-calculator/question.cpp
+++ project_5-calculator/question.cpp
@@ -3,18 +3,64 @@ using namespace std;
 
 void sum (int num1 , int num2){
 
-    cout<< "The sum of " << num1 << "&" << num2 <<" is: "<< num1+num2 << endl; 
+    cout<< "The sum of " << num1 << " & " << num2 <<" is: "<< num1+num2 << endl; 
+    cout  << endl;
 }
 
 void Subs (int num1 , int num2){
 
-    cout<< "The substraction of " << num1 << "&" << num2 <<" is: "<< num1-num2 << endl; 
+    int Subs;
+    Subs=num1-num2;
+    cout<< "The substraction of " << num1 << " & " << num2 <<" is: "<< Subs << endl; 
+    cout  << endl;
 }
+
+void Multiplication (int num1 , int num2){
+
+    cout<< "The Multiplication of " << num1 << "&" << num2 <<" is: "<< num1*num2 << endl; 
+    cout  << endl;
+}
+void Division (int num1 , int num2){
+
+    int div;
+    if (num1==0 || num2==0){
+
+        cout << "Number cannot be divided with zero.";
+        cout  << endl;
+    }else {
+
+        div=num1/num2;
+        cout<< "The Division of " << num1 << " & " << num2 <<" is: "<< div << endl; 
+        cout  << endl;
+    }
+}
+
+void modulas (int num1 , int num2){
+
+    int mod;
+    if (num1==0 || num2==0){
+
+        cout << "Number cannot be divided with zero."<< endl;
+        cout  << endl;
+    }else {
+
+        mod=num1%num2;
+        cout<< "The modulas of " << num1 << " & " << num2 <<" is: "<< mod << endl; 
+        cout  << endl;
+    }
+
+   
+}
+
+
 
 int main (){
 
     int choice , num1 , num2; 
 
+
+    while(choice != 0){
+    cout << "---------------------------------"<<endl;
     cout << "The choices : "<<endl;
     cout << "Press 1: for +  " << endl;
     cout << "Press 2: for - " << endl;
@@ -22,14 +68,16 @@ int main (){
     cout << "Press 4: for / " << endl;
     cout << "Press 5: for % " << endl;
     cout << "Press 0: for The Exit " << endl << endl;
+    
 
     cout << "Enter your choice: " ;
     cin >> choice;
 
     if (choice == 0) {
-        cout << "Exiting...\n";
+        cout << "You choose Exiting...\n";
         return 0;
     }
+    
 
 
     cout <<"Enter first number: ";
@@ -48,13 +96,24 @@ int main (){
         Subs( num1 ,  num2);
         break;
 
-        
+        case 3:
+        Multiplication( num1 ,  num2);
+        break;
+
+        case 4:
+        Division( num1 ,  num2);
+        break;
+
+        case 5:
+        modulas( num1 ,  num2);
+        break;
 
         default:
         cout<< "Invalid input..";
         break; 
 
 
+    }
     }
 
     return 0;
